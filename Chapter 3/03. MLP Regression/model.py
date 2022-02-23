39db61899ee1feed7cfda7dd38ae77005ff3c59c
alessiorecchia <recchia.alessio@yahoo.com>
2022-02-23 08:50:52 +0100

    uploaded C3 D3

---
+++ Chapter 3/03. MLP Regression/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torchsummary import summary
+
+# YOUR CODE HERE
+
+
+
+
+        
---
+++ Chapter 3/03. MLP Regression/train.py
@@ -0,0 +1,15 @@
+#import the needed libraries
+
+import data_handler as dh           # yes, you can import your code. Cool!
+import torch
+import torch.optim as optim
+import torch.nn as nn
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from model import #import your model here
+
+
+
+# Remember to validate your model: with torch.no_grad() ...... model.eval .........model.train
